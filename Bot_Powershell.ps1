9489e0869554025919fb6f89b383be2263cfdc37
MorganS42 <morgan@teamstoodley.com>
2019-08-23 12:41:47 +1000

    Update Bot_Powershell.ps1

--- Bot_Powershell.ps1
+++ Bot_Powershell.ps1
@@ -1,6 +1,6 @@
 Sleep 3 #wait so you can select the input box
 $n=0; #saving what file to look at
-$file=”Your_File” #change this to your download folder or wherever your browser downloads to. You may want to create a new file and set it as where your browser downloads to just for this program.
+$file=”Your_File”+"\"; #change this to your download folder or wherever your browser downloads to. You may want to create a new file and set it as where your browser downloads to just for this program.
 
 while(1) { #runs forever
     $t = New-Object -ComObject wscript.shell; #setting up the typing
